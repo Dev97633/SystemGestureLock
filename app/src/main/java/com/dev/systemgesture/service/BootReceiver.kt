7f2a26b941dff05d1d5685d2fbc3ec43d4b112b1
DEV <devendar87502@gmail.com>
2026-03-01 15:46:59 +0530

    Update BootReceiver.kt

--- app/src/main/java/com/dev/systemgesture/service/BootReceiver.kt
+++ app/src/main/java/com/dev/systemgesture/service/BootReceiver.kt
@@ -1,3 +1,9 @@
+package com.dev.systemgesture.service
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
 class BootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
